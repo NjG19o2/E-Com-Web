61c8b6a7087bfe4652d408bf78df7dd2e787581b
NjG19o2 <njg19o2@gmail.com>
2024-04-03 16:53:39 +0530

    Items.js disable view all button

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,24 +4,24 @@ import itemContext from "../context/items/itemContext";
 import './styles.css'
 
 const Items = (props) => {
-  const context =useContext(itemContext);
-  const {mode}=context;
-  return (
+ const context = useContext(itemContext);
+ const { mode } = context;
+ return (
     <div className="item">
-      <div className={`item card bg-${mode} text-${mode==='light'?'dark':'light'}`} style={{boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.08)" }} >
-        <img src={props.image}className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">
-            {props.description}
-          </p>
-          <Link to={props.link} className="btn btn-primary">
-            Go somewhere
-          </Link>
+      <Link to={props.link} style={{ textDecoration: 'none' }}>
+        <div className={`item card bg-${mode} text-${mode === 'light' ? 'dark' : 'light'}`} style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.08)" }}>
+          <img src={props.image} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">
+              {props.description}
+            </p>
+            
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
-  );
+ );
 };
 
 export default Items;
